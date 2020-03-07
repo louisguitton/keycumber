1daa223c62eb4f752adf12d6108da037fd5fc2ce
Louis Guitton <louisguitton93@gmail.com>
2020-03-07 11:51:13 +0100

    improve CLI UX

--- Readme.md
+++ Readme.md
@@ -8,19 +8,46 @@
 pip install keycumber
 ```
 
-## Example
+## Usage
+
+Get help
 
 ```sh
-keycumber --help
+$ keycumber --help
+Usage: keycumber [OPTIONS]
+
+  🥒   Combine Keywords from CSV files.
+
+Options:
+  -d, --destinations FILE  Path to the CSV files containing your destination
+                           names. It should contain 1 column called
+                           destination.  [required]
+  -m, --modifiers FILE     Path to the CSV files containing your modifiers. It
+                           should contain 1 column called kw.  [required]
+  -o, --out PATH           Path where to write the output of the script. It
+                           can be a directory or a file. Output will be of CSV
+                           format.  [required]
+  --help                   Show this message and exit.
+```
+
+Interactive mode
+
+```sh
+keycumber
+```
+
+Pass the filepaths directly
+
+```sh
 keycumber -d data/destinations.csv -m data/modifiers.csv -o data/output/
 ```
 
 ## Todo
 
-- add nice click descriptions
 - add order = pois_first / modifiers_first / both
 - add batch
-    if not specified, output everything in one file. if specified, default to 700 and creates files of 700 rows in folder
+  if not specified, output everything in one file. if specified, default to 700 and creates files of 700 rows in folder
+- add check for CSVs
 - add checks on files have only 1 column
 - add rename on file column to support files with other name
 - handle files with no headers
--- keycumber/__init__.py
+++ keycumber/__init__.py
@@ -7,11 +7,7 @@ import pandas as pd
 click_completion.init()
 
 
-def combine_keywords(
-    destinations="../data/destinations.csv",
-    modifiers="../data/modifiers.csv",
-    output="../data/output.csv",
-):
+def combine_keywords(destinations, modifiers, output):
     destinations = pd.read_csv(destinations)
     modifiers = pd.read_csv(modifiers)
 
@@ -27,16 +23,44 @@ def combine_keywords(
     # df['output_2'] = df.modifiers + ' ' + df.destinations
 
     # TODO: only one column, batch by 700 rows maximum
+
     Path(output).mkdir(parents=True, exist_ok=True)
     if Path(output).is_dir():
         output_path = Path(output) / "out.csv"
-    df.to_csv(output_path, index=False)
+    else:
+        output_path = output
+    df[["output"]].to_csv(output_path, index=False)
+    click.secho(
+        "Keywors successfully combined. Output stored in {}".format(output_path),
+        fg="green",
+    )
 
 
 @click.command()
-@click.option("--destinations", "-d", type=click.Path(exists=True, dir_okay=False))
-@click.option("--modifiers", "-m", type=click.Path(exists=True, dir_okay=False))
-@click.option("--out", "-o", type=click.Path(dir_okay=True, writable=True))
+@click.option(
+    "--destinations",
+    "-d",
+    required=True,
+    type=click.Path(exists=True, dir_okay=False),
+    prompt="Path to destination CSV",
+    help="Path to the CSV files containing your destination names. It should contain 1 column called destination.",
+)
+@click.option(
+    "--modifiers",
+    "-m",
+    required=True,
+    type=click.Path(exists=True, dir_okay=False),
+    prompt="Path to modifiers CSV",
+    help="Path to the CSV files containing your modifiers. It should contain 1 column called kw.",
+)
+@click.option(
+    "--out",
+    "-o",
+    required=True,
+    type=click.Path(dir_okay=True, writable=True),
+    prompt="Path to output CSV or directory",
+    help="Path where to write the output of the script. It can be a directory or a file. Output will be of CSV format.",
+)
 def cli(destinations, modifiers, out):
-    """🥒   Combine Keywords from files"""
+    """🥒   Combine Keywords from CSV files."""
     combine_keywords(destinations=destinations, modifiers=modifiers, output=out)
